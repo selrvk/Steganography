70ee7592dff70f04c1c35d07303eb55a6be937d6
Charles <charles.a7cantara@gmail.com>
2024-10-21 22:31:15 +0800

    encode binary done

--- src/main/java/com/selrvk/steganography/Controller.java
+++ src/main/java/com/selrvk/steganography/Controller.java
@@ -8,30 +8,27 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import javafx.scene.*;
-import javafx.event.*;
 import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Controller {
     @FXML
-    private Stage stage;
+    private ImageView imgView;
+    @FXML
+    private TextField inputField;
+
     private Scene scene;
     private Parent root;
-    @FXML
-    private ImageView imgView;
-    private TextField inputField;
 
     private File file;
 
     public void uploadImg(ActionEvent event){
 
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
         FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().addAll(
@@ -40,28 +37,35 @@ public class Controller {
         );
 
         File chosenFile = fileChooser.showOpenDialog(stage);
-        this.file = chosenFile;
+        setFile(chosenFile);
 
         if(chosenFile != null){
 
             Image myImage = new Image(chosenFile.toURI().toString());
             imgView.setImage(myImage);
-            readImage(chosenFile);
         }
 
     }
 
-    public void readImage(File file){
+    public void encode(){
+
+        int binIndex = 0;
 
         try{
 
-            BufferedImage img = ImageIO.read(file);
+            String bin = convertToBinary(inputField.getText());
+            BufferedImage img = ImageIO.read(getFile());
             int w = img.getWidth();
             int h = img.getHeight();
 
+            outerLoop:
             for(int y = 0 ; y < h ; y++) {
 
                 for (int x = 0; x < w; x++) {
+
+                    if(binIndex >= bin.length()){
+                        break outerLoop;
+                    }
 
                     int pixel = img.getRGB(x, y);
                     int alpha = (pixel >> 24) & 0xff;
@@ -69,35 +73,95 @@ public class Controller {
                     int green = (pixel >> 8) & 0xff;
                     int blue = pixel & 0xff;
 
-                    if (x == 0 && y == 0) {
-                        System.out.println("Top left pixel: R= " + red + "  G= " + green + "  B= " + blue + "  Alpha= " + alpha);
-                    }
+                    blue = (blue & 0xFE) | (bin.charAt(binIndex) - '0');
+                    binIndex++;
+
+                    int newPixel = (0xff << 24) | (red<<16) | (green << 8) | blue;
+                    img.setRGB(x,y,newPixel);
+
+                    exportImage(img);
                 }
             }
 
-        } catch (IOException e){
-
+        } catch (Exception e){
 
             e.printStackTrace();
 
         }
     }
-    public void encode(){
-
-
-        System.out.println("Encode pressed");
-
-    }
 
     public String convertToBinary(String string){
 
-        return "uwu";
+        StringBuilder bin = new StringBuilder();
+        for(char c : string.toCharArray()){
+
+            bin.append(String.format("%8s", Integer.toBinaryString(c)).replaceAll(" ", "0"));
+        }
+
+        return bin.toString();
+    }
+
+    public void exportImage(BufferedImage image){
+
+        try {
+
+            File exportLoc = new File("src/testuwu.png");
+            ImageIO.write(image, "png", exportLoc);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
     }
 
     public void decode(){
 
-        System.out.println("Decode pressed");
+        StringBuilder bin = new StringBuilder();
 
+        try{
+
+            BufferedImage img = ImageIO.read(getFile());
+            int w = img.getWidth();
+            int h = img.getHeight();
+
+            outerLoop:
+            for(int y = 0 ; y < h ; y++) {
+
+                for (int x = 0; x < w; x++) {
+
+                    if(bin.length() >= 48){
+                        break outerLoop;
+                    }
+
+                    int pixel = img.getRGB(x, y);
+
+                    int blue = pixel & 0xff;
+                    
+                    bin.append(blue & 1);
+
+                }
+            }
+
+        } catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+    public void setFile(File file){
+
+        this.file = file;
+
+    }
+
+    public File getFile(){
+
+        return this.file;
     }
 
 }
